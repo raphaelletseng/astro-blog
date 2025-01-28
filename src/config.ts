11b90a4e1af02298533e17b9a81a5abfd4aea089
Raphaelle Tseng <raphaelletseng@gmail.com>
2025-01-27 21:40:49 -0500

    trema

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "raphaelle tsengzr",
+  title: "raphaëlle tsengzr",
   subtitle: "Raphaelle Tseng's bloggy blog",
   lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
