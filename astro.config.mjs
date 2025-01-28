4ae6a2c829ed649a4f5ffdff547a9564cbaff2d9
Raphaelle Tseng <raphaelletseng@gmail.com>
2025-01-27 19:01:19 -0500

    ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raphaelletseng.github.io',
-  base: '/',
+  base: '',
   trailingSlash: 'always',
   integrations: [
     tailwind({
